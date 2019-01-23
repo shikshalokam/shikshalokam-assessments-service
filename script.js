4089f70ab18d7a5cbbd05a4426116024560fc631
aman <aman@tunerlabs.com>
2019-01-23 23:21:23 +0530

    optimized script for changing parent type

--- script.js
+++ script.js
@@ -1,43 +1,16 @@
 let MongoClient = require("mongodb").MongoClient;
-let url = "mongodb://localhost:27017/";
+let url = "mongodb://localhost:27017/sl-assessment";
 
-MongoClient.connect(url, (err, db) => {
-    if (err) { throw err }
-    console.log("Connected to database!!");
-    let databaseName = db.db("sl-assessment")
-    const parentRegistryCollection = databaseName.collection("parent-registry")
-    parentRegistryCollection.find({}).toArray((err, parentRegistryDoc) => {
-        if (err) throw err;
-        let index = 0
-        findAndUpdate(index)
-
-        function findAndUpdate(index) {
-            if (parentRegistryDoc[index]) {
-                parentRegistryCollection.find({ _id: parentRegistryDoc[index]._id }).toArray((err, eachParentRegistryData) => {
-                    if (err) throw err
-                    if (!eachParentRegistryData) {
-                        console.log("Id is not matched")
-                        index++;
-                        findAndUpdate(index)
-                    }
-                    else {
-                        let findQuery = { _id: parentRegistryDoc[index]._id }
-                        eachParentRegistryData['type'] = []
-                        eachParentRegistryData.forEach(eachParent => {
-                            eachParentRegistryData.type.push(eachParent.type)
-                        })
-                        let updateQuery = { $set: { type: eachParentRegistryData.type } }
-                        parentRegistryCollection.updateOne(findQuery, updateQuery).then(() => {
-                            index++;
-                            findAndUpdate(index)
-                            if (!parentRegistryDoc[index]) {
-                                db.close()
-                            }
-
-                        })
-                    }
-                })
-            }
-        }
+MongoClient.connect(url, function (err, db) {
+    let dbo = db.db("sl-assessment");
+    const SubmissionCollection = dbo.collection("parent-registry")
+    SubmissionCollection.find({}).project({ type: 1 }).toArray((err, submissionData) => {
+        submissionData.forEach(eachSubmission => {
+            let findQuery = { _id: eachSubmission._id }
+            let updateQuery = { $set: { type: [eachSubmission.type] } }
+            SubmissionCollection.updateOne(findQuery, updateQuery).then(() => {
+                console.log("updated successfully")
+            })
+        })
     })
-})
+});
