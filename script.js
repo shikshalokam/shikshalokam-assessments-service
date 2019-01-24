fa09f9c2b440c1d042bd3846fa9eb12e29e4aa46
aman <aman@tunerlabs.com>
2019-01-24 11:04:09 +0530

    changed variable name from submissionCollection to parentRegistryCollection

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let url = "mongodb://localhost:27017/sl-assessment";
 
 MongoClient.connect(url, function (err, db) {
     let dbo = db.db("sl-assessment");
-    const SubmissionCollection = dbo.collection("parent-registry")
-    SubmissionCollection.find({}).project({ type: 1 }).toArray((err, submissionData) => {
-        submissionData.forEach(eachSubmission => {
-            let findQuery = { _id: eachSubmission._id }
-            let updateQuery = { $set: { type: [eachSubmission.type] } }
-            SubmissionCollection.updateOne(findQuery, updateQuery).then(() => {
+    const parentRegistryCollection = dbo.collection("parent-registry")
+    parentRegistryCollection.find({}).project({ type: 1 }).toArray((err, parentRegistryData) => {
+        parentRegistryData.forEach(eachParent => {
+            let findQuery = { _id: eachParent._id }
+            let updateQuery = { $set: { type: [eachParent.type] } }
+            parentRegistryCollection.updateOne(findQuery, updateQuery).then(() => {
                 console.log("updated successfully")
             })
         })
