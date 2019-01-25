aa8c033e9103f6a37f524126fc02cfa202637be8
aman <aman@tunerlabs.com>
2019-01-25 11:13:10 +0530

    added submissions in evidencesStatus without answers

--- currentDateScript.js
+++ currentDateScript.js
@@ -8,18 +8,17 @@ MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
     submissionsCollection.find({}).project({ evidences: 1 }).toArray((err, submissionData) => {
 
         submissionData.forEach(eachSubmission => {
-            eachSubmission['evidenceSubmissions'] = []
+            eachSubmission['evidenceStatus'] = []
             Object.values(eachSubmission.evidences).forEach(singleEvidence => {
+                eachSubmission.evidenceStatus.push(singleEvidence)
                 if (singleEvidence.submissions) {
                     singleEvidence.submissions.forEach(eachIndividualSubmission => {
                         delete eachIndividualSubmission.answers
-                        eachSubmission.evidenceSubmissions.push(eachIndividualSubmission);
                     });
                 }
-
             })
             let findQuery = { _id: eachSubmission._id }
-            let updateQuery = { $set: { evidenceSubmissions: eachSubmission.evidenceSubmissions } }
+            let updateQuery = { $set: { evidencesStatus: eachSubmission.evidenceStatus } }
             submissionsCollection.updateOne(findQuery, updateQuery).then(() => {
                 console.log("updated successfully")
             })
