180847274256c13d1a0c6d5faadfae59e477bb16
Akash Shah <akash.shah@above-inc.com>
2019-02-11 11:01:46 +0530

    Intermediate commit.

--- controllers/migrationStatusController.js
+++
@@ -1,8 +0,0 @@
-module.exports = class MigrationStatus extends Abstract {
-    constructor(schema) {
-      super(schema);
-    }
-    static get name() {
-        return "migrationStatus";
-      }
-}
