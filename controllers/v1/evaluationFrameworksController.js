1cb757b983baa554772fe723c3e2904f5288c1f6
aman-tunerlabs <aman@tunerlabs.com>
2019-04-01 23:09:02 +0530

    framework to rubric added

--- controllers/v1/evaluationFrameworksController.js
+++ controllers/v1/evaluationFrameworksController.js
@@ -19,7 +19,6 @@ module.exports = class EvaluationFrameworks extends Abstract {
     return super.findById(req);
   }
 
-
   async evaluationFrameworkDocument(evaluationIds = "all", fields = "all") {
 
     let queryObject = {}
@@ -43,5 +42,160 @@ module.exports = class EvaluationFrameworks extends Abstract {
 
     let evaluationFrameworkDocuments = await database.models["evaluationFrameworks"].find(queryObject, projectionObject);
     return evaluationFrameworkDocuments
+  }
+
+  async details(req) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let criteriaDocument = await database.models.criterias.find({},{"name":1,"rubric.levels":1}).lean()
+
+        let criteriaObject = {}
+
+        criteriaDocument.forEach(eachCriteria=>{
+          let levelsDescription = {}
+
+          for(let k in eachCriteria.rubric.levels){
+            levelsDescription[k] = eachCriteria.rubric.levels[k].description
+          }
+          criteriaObject[eachCriteria._id.toString()] =_.merge({
+            name:eachCriteria.name
+          },levelsDescription) 
+        })
+
+        let findQuery = {
+          externalId:req.params._id
+        }
+
+        let evaluationFrameworkDocument = await database.models.evaluationFrameworks.findOne(findQuery,{themes:1,levelToScoreMapping:1,name:1}).lean()
+        
+        let responseObject = {}
+        responseObject.heading = "Framework + rubric api for - "+evaluationFrameworkDocument.name
+        responseObject.summary = [
+          {
+           name:"",
+           title:"" 
+          }
+        ]
+
+        responseObject.sections = new Array
+
+        let levelValue = {}
+
+        let sectionHeaders = new Array
+        
+        let criteriaNameObject = {
+          key:"criteriaName",
+          value:"Domain"
+        }
+
+        sectionHeaders.push(criteriaNameObject)
+
+        for(let k in evaluationFrameworkDocument.levelToScoreMapping){
+          levelValue[k]=""
+          sectionHeaders.push({name: k,value:evaluationFrameworkDocument.levelToScoreMapping[k].label})
+        }
+
+        let generateSections = function(eachDocument){
+          
+          if(eachDocument.criteriaName){
+            let tableData = new Array
+            
+            Object.values(eachDocument.criteriaName).forEach(eachData=>{
+              let levelObjectFromCriteria={}
+
+              eachData.forEach(eachCriteria=>{
+                Object.keys(levelValue).forEach(eachLevel=>{
+                  levelObjectFromCriteria[eachLevel] = criteriaObject[eachCriteria.criteriaId.toString()][eachLevel]
+                })
+                tableData.push(_.merge({
+                  criteriaName:criteriaObject[eachCriteria.criteriaId.toString()].name,
+                },levelObjectFromCriteria))
+              })
+            })
+
+            let eachSection = {
+              table: true,
+              heading: "",
+              data: tableData,
+              tabularData: {
+                headers: sectionHeaders
+              },
+              // summaryData:parentData
+            }
+
+            responseObject.sections.push(eachSection)
+          }
+          else{
+            let remainingDocument = _.omit(eachDocument,["name","label"])
+            Object.keys(remainingDocument).forEach(eachValue => {
+              if (eachValue != "criteriaName") {
+                generateSections(eachDocument[eachValue])
+              }
+            })
+          }
+        }
+
+        evaluationFrameworkDocument.themes.forEach(eachTheme=>{
+         
+         let criteriaDoc = this.getCriteriaPath(eachTheme.children,eachTheme.name,eachTheme.label)
+         
+         Object.keys(criteriaDoc).forEach(eachKey=>{
+          let eachDoc = criteriaDoc[eachKey]
+          generateSections(eachDoc)
+         })
+
+        })
+        
+        let response = {
+          message: "Framework + rubric api fetched successfully.",
+          result: responseObject
+        };
+
+        return resolve(response);
+      } catch (error) {
+        return reject({
+          status: 500,
+          message: error,
+          errorObject: error
+        });
+      }
+    });
+  }
+
+  getCriteriaPath(themes,name,label) {
+
+    if(!this.path) this.path = {}
+    
+    themes.forEach(theme => {
+
+      if(!this.parent) this.parent= new Array
+
+      if(typeof name == "string") {
+        
+        if(!this.path[name]) this.path[name] = {
+          name:name,
+          label:label
+        }
+
+        this.path[name][theme.name] = {
+          name:theme.name,
+          label:theme.label
+        }
+      }
+      
+      else name[theme.name] = {
+        name:theme.name,
+        label:theme.label
+      }
+      
+      if (theme.children) {
+        this.getCriteriaPath(theme.children,this.path[name][theme.name]);
+      } else{
+        name[theme.name]["criteriaName"] = {
+          criterias:theme.criteria
+        }
+      }
+    })
+    return this.path;
   }
 };
