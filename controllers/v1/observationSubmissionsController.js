0c672ca56b4e8462c25abd4d3d94285a9a28d0cd
aman-tunerlabs <aman@tunerlabs.com>
2019-12-23 10:43:21 +0530

    observation submissions calculating weightage score even if question score is zero

--- controllers/v1/observationSubmissionsController.js
+++ controllers/v1/observationSubmissionsController.js
@@ -696,7 +696,12 @@ module.exports = class ObservationSubmissions extends Abstract {
                 if(question.responseType == "multiselect") {
                   questionMaxScore += option.score
                 }
-                (option.score && option.score > 0) ? submissionDocument.questionDocuments[question._id.toString()][`${option.value}-score`] = option.score : ""
+
+                if ("score" in option) {
+                  option.score >= 0 ? 
+                  submissionDocument.questionDocuments[question._id.toString()][`${option.value}-score`] =
+                   option.score : "";
+                }
               })
             }
             if(question.sliderOptions && question.sliderOptions.length > 0) {
@@ -858,7 +863,15 @@ module.exports = class ObservationSubmissions extends Abstract {
                 if(question.responseType == "multiselect") {
                   questionMaxScore += option.score
                 }
-                (option.score && option.score > 0) ? commonSolutionDocumentParameters.questionDocuments[question._id.toString()][`${option.value}-score`] = option.score : ""
+
+                if("score" in option) {
+
+                  option.score >= 0 ? 
+                  commonSolutionDocumentParameters.questionDocuments[question._id.toString()][`${option.value}-score`] = 
+                  option.score 
+                  : "";
+                }
+                
               })
             }
             if(question.sliderOptions && question.sliderOptions.length > 0) {
